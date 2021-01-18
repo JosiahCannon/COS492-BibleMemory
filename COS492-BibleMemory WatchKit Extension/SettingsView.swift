1217282df3d773f51f09f33e385b28c2213e5296
josiahcannon <jrolfecannon@gmail.com>
2021-01-18 16:27:21 -0500

    Added “Daily” Toggle to SettingsView.swift. Also designed the “settings” page and added an SFSymbol to “fancy-up” the toggle

--- COS492-BibleMemory WatchKit Extension/SettingsView.swift
+++ COS492-BibleMemory WatchKit Extension/SettingsView.swift
@@ -5,12 +5,41 @@
 //  Created by Josiah Cannon on 1/4/21.
 //
 
+//ADD ME: Daily notification toggle SHOULD have functionality (i.e. control daily push notifications/reminders
+
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var isNotifOn: Bool = false
+    
     var body: some View {
-        Text("Test SettingsView")
+        VStack {
+            NotificationToggle(isNotifOn: $isNotifOn)
+        }
     }
+}
+
+struct NotificationToggle: View {
+    @Binding var isNotifOn: Bool
+
+    var body: some View {
+        VStack {
+            Toggle(isOn: $isNotifOn) {
+                Image(systemName: "clock.arrow.2.circlepath").foregroundColor(isNotifOn ? .green : .white)
+                
+                Text("Daily").foregroundColor(.white)
+            }
+            .padding(.vertical, 3.0).foregroundColor(isNotifOn ? .green : .gray)
+            .padding(.trailing, 5.0)
+            
+            Divider()
+                .background(Color.gray).frame(width: 150.0, height: 1.0)
+        }
+        .navigationTitle(Text("Settings")) //CHECK ME: change string?
+        
+        Spacer()
+    }
+    
 }
 
 struct SettingsView_Previews: PreviewProvider {
