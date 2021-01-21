a1c9ec4ff0ff97f2f7a83594dff701a3f20c9bf0
josiahcannon <jrolfecannon@gmail.com>
2021-01-20 20:02:00 -0500

    Expanded NotificationView.swift; it displays image, “DAILY REMINDER”, and “Review Scripture” (Note: this file is NOT used) Added “Authorize Notification” button to SettingsView.swift; only displays if user has not authorized notifications; when pressed, it displays a prompt for the user to Accept or Deny permission to send notifications. This button disappears and is replaced with “NotificationToggle” once notifications are authorized for the user. Added func “scheduleDaily” which holds all the code for creating a notification. This func is called every time NotificationToggle is toggled. If NotificationToggle is toggled ON (i.e. if isNotifOn == true), scheduleDaily() adds a new notification request (my notifications are set to repeat everyday at 9:00AM. If NotificationToggle is toggled OFF (i.e. if isNotifOn == false), scheduleDaily() removes all pending notifications. Updated struct NotificationToggle’s code to call scheduleDaily() anytime isNotifOn’s value changes (i.e. anytime the user turns the Toggle on OR off)

--- COS492-BibleMemory WatchKit Extension/NotificationView.swift
+++ COS492-BibleMemory WatchKit Extension/NotificationView.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct NotificationView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+//            Image("white-bible-icon_4")
+//                .resizable()
+//                .frame(width: 70.0, height: 60.0)
+            
+            Text("DAILY REMINDER")
+                .multilineTextAlignment(.center).foregroundColor(.accentColor)
+            Divider()
+            Text("Review Scripture")
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
-        NotificationView()
+        ForEach(["Apple Watch Series 3 - 38mm", "Apple Watch Series 6 - 44mm"],
+                id: \.self) {
+            deviceName in NotificationView()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
--- COS492-BibleMemory WatchKit Extension/SettingsView.swift
+++ COS492-BibleMemory WatchKit Extension/SettingsView.swift
@@ -5,16 +5,61 @@
 //  Created by Josiah Cannon on 1/4/21.
 //
 
-//ADD ME: Daily notification toggle SHOULD have functionality (i.e. control daily push notifications/reminders
-
 import SwiftUI
+import UserNotifications
+
+func scheduleDaily(inputBool: Bool) {
+    let notifBool: Bool = inputBool
+    
+    let content = UNMutableNotificationContent()
+    content.title = "Daily Reminder"
+    content.body = "Review Scripture today!"
+    content.sound = UNNotificationSound.default
+
+    var dateComponents = DateComponents()
+    dateComponents.hour = 19
+    dateComponents.minute = 47
+    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+    //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false) //REMOVE ME - DEBUG
+    
+    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+    
+    if(notifBool) {
+        print("DEBUG1 notifBool: \(notifBool)") //DEBUG
+        UNUserNotificationCenter.current().add(request)
+    }
+    else {
+        print("DEBUG2 notifBool: \(notifBool)") //DEBUG
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+    }
+}
 
 struct SettingsView: View {
+    @AppStorage("Authorized") private var isAuthorized: Bool = false
     @AppStorage("Daily") private var isNotifOn: Bool = false
+
+    func authorizeNotifs() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if success {
+                isAuthorized = true
+                print("User Accepted")
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
     
     var body: some View {
         VStack {
-            NotificationToggle(isNotifOn: $isNotifOn)
+            if(!isAuthorized) {
+                Button("Authorize Notifications") {
+                    authorizeNotifs()
+                }.padding(.top, 5.0).foregroundColor(.green).multilineTextAlignment(.center)
+            }
+            
+            if(isAuthorized) {
+                NotificationToggle(isNotifOn: $isNotifOn)
+            }
         }
         .navigationTitle(Text("Settings")) //CHECK ME: change string?
     }
@@ -29,14 +74,16 @@ struct NotificationToggle: View {
                 Image(systemName: "clock.arrow.circlepath").foregroundColor(isNotifOn ? .green : .white)
                 
                 Text("Daily").foregroundColor(.white)
-            }
+            }.onChange(of: isNotifOn, perform: { value in
+                print("DEBUG isNotifOn: \(isNotifOn)")
+                scheduleDaily(inputBool: isNotifOn)
+            })
             .padding(.vertical, 3.0).foregroundColor(isNotifOn ? .green : .gray)
             .padding(.trailing, 5.0)
             
             Divider()
                 .background(Color.gray).frame(width: 200.0, height: 1.0)
         }
-        
         Spacer()
     }
     
